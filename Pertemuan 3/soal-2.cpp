e17b0120d6401b32994335c3fac3c8ff616c58c7
Danar Fathurahman <63245894+danarfthr@users.noreply.github.com>
2024-09-10 07:12:39 +0000

    Ne

---
+++ Pertemuan 3/soal-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int terbesar;
+
+    cout << "Inputkan bilangan 1: ";
+    cin >> a;
+
+    cout << "Inputkan bilangan 2: ";
+    cin >> b;
+
+    if (a > b) {
+        terbesar = a;
+    }
+    else if (a < b) {
+        terbesar = b;
+    }
+
+    cout << "Inputkan bilangan 3: ";
+    cin >> c;
+
+    if (terbesar < c) {
+        terbesar = c;
+    }
+
+    cout << "Bilangan terbesar adalah: " << terbesar << endl;
+
+
+    return 0;
+}
