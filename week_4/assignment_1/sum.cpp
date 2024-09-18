aab5563e89bcaa4e2e2d15cd9a6d2ec2b06f5128
Danar Fathurahman <63245894+danarfthr@users.noreply.github.com>
2024-09-18 05:23:01 +0000

    Update assignment 1

--- week_4/assignment_1/sum.cpp
+++ week_4/assignment_1/sum.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    int n;
+int sum(int n) {
     int jumlah = 0;
-
-    cout << "Masukkan angka n: ";
-    cin >> n;
-
     for (int i = 1; i <= n; i++) {
         jumlah = jumlah + i;
     }
+    return jumlah;
+}
 
-    cout << "Hasil: " << jumlah << endl;
+int main(int argc, char* argv[]) {
 
-    return 0;
+    int n;
+
+    if (argc > 1) {
+        if (string(argv[1]) == "test") {
+            ifstream testFile("test.txt");
+            string expected_output;
+            
+            if (!testFile) {
+                cerr << "Error: Tidak dapat membuka test.txt" << endl;
+                return 1;
+            }
+
+            int test_num = 1;
+            cout << "------------------------" << endl;
+            while (testFile >> n && getline(testFile >> ws, expected_output)) {
+                int jumlah = sum(n);
+                if (to_string(jumlah) == expected_output) {
+                    cout << "Test " << test_num << " passed!" << endl;
+                    cout << "Input : " << n << endl;
+                    cout << "Output : " << jumlah << endl;
+                } else {
+                    cout << "Test " << test_num << " failed." << endl;
+                    cout << "Input : " << n << endl;
+                    cout << "Output : " << jumlah << endl;
+                    cout << "/Expected Output : " << expected_output << "/" << endl;
+                }
+                cout << "------------------------" << endl;
+                test_num++;
+            }
+            testFile.close();
+            return 0;
+        }
+    }
+    else {
+        cout << "Masukkan angka: ";
+        cin >> n;
+        cout << sum(n) << endl;
+        return 0;
+    }
 }
