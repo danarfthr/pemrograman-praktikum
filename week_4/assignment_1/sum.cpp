bd1fcf02f61247795d55e1ffc20e84de4a32a19b
Danar Fathurahman <63245894+danarfthr@users.noreply.github.com>
2024-09-22 13:46:10 +0000

    Add assignment_2

--- week_4/assignment_1/sum.cpp
+++ week_4/assignment_1/sum.cpp
@@ -15,42 +15,9 @@ int sum(int n) {
 int main(int argc, char* argv[]) {
 
     int n;
-
-    if (argc > 1) {
-        if (string(argv[1]) == "test") {
-            ifstream testFile("test.txt");
-            string expected_output;
-            
-            if (!testFile) {
-                cerr << "Error: Tidak dapat membuka test.txt" << endl;
-                return 1;
-            }
-
-            int test_num = 1;
-            cout << "------------------------" << endl;
-            while (testFile >> n && getline(testFile >> ws, expected_output)) {
-                int jumlah = sum(n);
-                if (to_string(jumlah) == expected_output) {
-                    cout << "Test " << test_num << " passed!" << endl;
-                    cout << "Input : " << n << endl;
-                    cout << "Output : " << jumlah << endl;
-                } else {
-                    cout << "Test " << test_num << " failed." << endl;
-                    cout << "Input : " << n << endl;
-                    cout << "Output : " << jumlah << endl;
-                    cout << "/Expected Output : " << expected_output << "/" << endl;
-                }
-                cout << "------------------------" << endl;
-                test_num++;
-            }
-            testFile.close();
-            return 0;
-        }
-    }
-    else {
-        cout << "Masukkan angka: ";
-        cin >> n;
-        cout << sum(n) << endl;
-        return 0;
-    }
+    cout << "Masukkan angka: ";
+    cin >> n;
+    cout << sum(n) << endl;
+    
+    return 0;
 }
---
+++ week_4/assignment_2/multiplication.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void multiplication(int n) {
+    int total = 10;
+    cout << "Input: " << n << endl;
+    cout << "Output: " << endl;
+    for (int i = 1; i <= total; i++) {
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int n;
+    cout << "Input : ";
+    cin >> n;
+    cout << "Output: " << endl;
+    multiplication(n);
+}
