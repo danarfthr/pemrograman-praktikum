1594557548507adf3aea491b2002b273bedd3f98
Danar Fathurahman <63245894+danarfthr@users.noreply.github.com>
2024-09-23 23:28:41 +0000

    Edit multiplication

--- week_4/assignment_2/multiplication.cpp
+++ week_4/assignment_2/multiplication.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 void multiplication(int n) {
     int total = 10;
-    cout << "Input: " << n << endl;
-    cout << "Output: " << endl;
     for (int i = 1; i <= total; i++) {
         cout << n << " x " << i << " = " << n*i << endl;
     }
