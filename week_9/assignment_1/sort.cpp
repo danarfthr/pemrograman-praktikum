bc09785f2ffcde7547fcff7097d39daa38f24e89
Danar Fathurahman <63245894+danarfthr@users.noreply.github.com>
2024-11-21 15:36:34 +0000

    Make week_9

---
+++ week_9/assignment_1/sort.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Deklarasi tipe data mahasiswa dengan struct
+struct Mahasiswa {
+    long int nisn; // NISN2
+    string nama; // Nama
+    int nilai; // Nilai
+};
+
+void insertionSortNISN(Mahasiswa mhs[], int size_array) {
+    for (int j = 1; j < size_array; j++) {
+        int i = j-1;
+        Mahasiswa temp = mhs[j];
+        while (i >= 0 && mhs[i].nisn > temp.nisn) {
+            mhs[i+1] = mhs[i];
+            i--;
+        }
+        mhs[i+1] = temp;
+    }
+}
+
+void insertionSortNilai(Mahasiswa mhs[], int size_array) {
+    for (int j = 1; j < size_array; j++) {
+        int i = j-1;
+        Mahasiswa temp = mhs[j];
+        while (i >= 0 && mhs[i].nilai > temp.nilai) {
+            mhs[i+1] = mhs[i];
+            i--;
+        }
+        mhs[i+1] = temp;
+    }
+}
+
+void selectionSortNISN(Mahasiswa mhs[], int size_array) {
+}
+
+int main() {
+    // Deklarasi data mahasiswa
+    Mahasiswa mhs[7] = {    
+        {9960312699, "Handi Ramadhan", 90},
+        {9963959682, "Rio Alfandra", 55},
+        {9950310962, "Ronaldo Valentino Uneputty", 80},
+        {9970272750, "Achmad Yaumil Fadjri R.", 60},
+        {9970293945, "Alivia Rahma Pramesti", 70},
+        {9952382180, "Ari Lutfianto", 65},
+        {9965653989, "Arief Budiman", 60}
+    };
+
+    int size_array = sizeof(mhs) / sizeof(mhs[0]);
+
+    int pilihan_data;
+    int pilihan_sort;
+    cout << "Pilih data apa yang akan diurutkan" << endl;
+    cout << "1. NISN" << endl;
+    cout << "2. Nilai" << endl;
+    cout << "Masukkan pilihan (1-2): ";
+    cin >> pilihan_data;
+
+    switch (pilihan_data)
+    {
+        case 1: // NISN
+            cout << "==========================" << endl;
+            cout << "Pilih cara sorting yang diinginkan" << endl;
+            cout << "1. Insertion Sort" << endl;
+            cout << "2. Selection Sort" << endl;
+            cout << "3. Bubble Sort" << endl;
+            cout << "Masukkan pilihan (1-3): ";
+            cin >> pilihan_sort;
+
+            switch (pilihan_sort)
+            {
+                case 1: // Insertion Sort
+                cout << "==========================" << endl;
+                
+                cout << "Before: " << endl;
+                for (int i = 0; i < size_array; i++) {
+                    cout << i << ": " << mhs[i].nisn << endl;
+                }
+
+                insertionSortNISN(mhs, size_array);
+
+                cout << "After: " << endl;
+                for (int i = 0; i < size_array; i++) {
+                    cout << i << ": " << mhs[i].nisn << endl;
+                }
+
+                break;
+
+
+
+                case 2: // Selection Sort
+                cout << "==========================" << endl;
+                
+                                cout << "Before: " << endl;
+                for (int i = 0; i < size_array; i++) {
+                    cout << i << ": " << mhs[i].nisn << endl;
+                }
+
+                break;
+
+
+
+                case 3: // Bubble Sort
+                cout << "==========================" << endl;
+
+
+                break;
+            }
+            break;
+        
+        case 2: // Nilai
+            cout << "==========================" << endl;
+            cout << "Pilih cara sorting yang diinginkan" << endl;
+            cout << "1. Insertion Sort" << endl;
+            cout << "2. Selection Sort" << endl;
+            cout << "3. Bubble Sort" << endl;
+            cout << "Masukkan pilihan (1-3): ";
+            cin >> pilihan_sort;
+
+            switch (pilihan_sort)
+            {
+                case 1: // Insertion Sort
+                cout << "==========================" << endl;
+                
+                cout << "Before: " << endl;
+                for (int i = 0; i < size_array; i++) {
+                    cout << i << ": " << mhs[i].nilai << endl;
+                }
+
+                insertionSortNilai(mhs, size_array);
+
+                cout << "After: " << endl;
+                for (int i = 0; i < size_array; i++) {
+                    cout << i << ": " << mhs[i].nilai << endl;
+                }
+
+
+
+                break;
+
+                
+
+                case 2: // Selection Sort
+                cout << "==========================" << endl;
+
+
+                break;
+
+
+
+                case 3: // Bubble Sort
+                cout << "==========================" << endl;
+
+
+                break;
+            }
+            break;
+
+    }
+}
